c7382d41ed922b2bc22348dd34fd23a8874828fe
Larissa Andrade <larissa.sandrade@hotmail.com>
2023-06-16 13:57:40 -0300

    feat: adicionar descricao do tipo da transferencia

--- ContaCorrente/TipoTransferencia.java
+++ ContaCorrente/TipoTransferencia.java
@@ -1,7 +1,18 @@
 package ContaCorrente;
 
 public enum TipoTransferencia {
-    TED,
-    DOC,
-    PIX
+    TED("Transferência Eletrônica Disponível"),
+    DOC("Documento de Ordem de Crédito"),
+    PIX("Pagamento Instantâneo");
+
+    private String descricao;
+
+    TipoTransferencia(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
 }
